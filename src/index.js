8d26bfde70546a12b41e1235e75564d0ff7ed515
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2024-01-22 00:56:30 +0530

    writing basics

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import connectDB from "./DB/index.js";
+import app from "./app.js";
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,
+        ()=>{
+            console.log(`server is running on port: ${process.env.PORT}`);
+        })
+})
+.catch((err)=>{
+    console.log("error while connecting database", err);
+})
+
+export default connectDB;
